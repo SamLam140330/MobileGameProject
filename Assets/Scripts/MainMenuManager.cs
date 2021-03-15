6df966ef71c96893f9e8ffc964e3c35f8b237b6b
SamLam140330 <lamlam0328@gmail.com>
2021-03-15 17:05:44 +0800

    Continue to edit the game and add a menu scene

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnScoreButtonClicked()
+    {
+
+    }
+
+    public void OnAudioButtonClicked()
+    {
+
+    }
+
+    public void OnShareButtonClicked()
+    {
+
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
