3dcc77c099dccba0bb47fbb926c620b10902310b
SamLam140330 <lamlam0328@gmail.com>
2021-04-26 18:44:27 +0800

    Bug fix

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -2,11 +2,21 @@
 
 public class BulletController : MonoBehaviour
 {
+    private ObjectSpawnManager objectSpawnManager = null;
+
+    private void Start()
+    {
+        objectSpawnManager = FindObjectOfType<ObjectSpawnManager>();
+    }
+
     private void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += 6f * Time.deltaTime;
-        transform.position = pos;
+        if (objectSpawnManager.isGameOver == false)
+        {
+            Vector3 pos = transform.position;
+            pos.x += 6f * Time.deltaTime;
+            transform.position = pos;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
